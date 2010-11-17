aa3707d300e44149e5a0a5271b26aabb811f49c6
balazs <balazs@985d44b5-3378-4232-8656-f57e795d5726>
2010-11-17 20:09:32 +0000

    Moving to udunits2.

--- common.mk
+++ common.mk
@@ -3,14 +3,14 @@ UNIX=$(shell uname)
 ifeq ($(UNIX),Linux)
 export UNIXCC=gcc
 export UNIXCCOPS=-g -Wall -fsigned-char -D_GNU_SOURCE -fPIC -I/usr/local/include
-export UNIXLIBS=-rdynamic -L/usr/local/lib -lexpat -ldl -ludunits2 -lm
-export UNIXPLGLNK=-shared -nostartfiles -L/usr/local/lib -lnetcdf
+export UNIXLIBS=-rdynamic -lexpat -ldl -ludunits2 -lm
+export UNIXPLGLNK=-shared -nostartfiles -lnetcdf
 export UNIXMAKE=make
 endif
 ifeq ($(UNIX),Darwin)
 export UNIXCC=gcc
 export UNIXCCOPS=-g -Wall -fsigned-char -D_GNU_SOURCE -I/sw/include
-export UNIXLIBS=-L/usr/local/lib -lexpat -ldl -ludunits2 -lm
+export UNIXLIBS=-L/sw/lib -lexpat -ldl -ludunits2 -lm
 export UNIXPLGLNK=-dynamiclib -flat_namespace -undefined suppress -L/sw/lib -lnetcdf
 export UNIXMAKE=make
 endif
